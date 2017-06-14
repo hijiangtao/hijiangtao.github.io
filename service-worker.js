e0f28a4bc19488cf6cabec99652875f4a351e239
Joe Jiang <hijiangtao@gmail.com>
2017-06-14 23:23:39 +0800

    init: seervice-worker

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+/**
+ * service-worker.js
+ * @authors Joe Jiang (hijiangtao@gmail.com)
+ * @date    2017-06-14 23:22:15
+ * @version $Id$
+ */
+
+var dataCacheName = 'data-v1';
+var cacheName = 'PWA-6-14';
+var filesToCache = [
+];
